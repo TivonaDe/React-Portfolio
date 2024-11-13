7a50f4cf039ef9f48d4a88efb2e2e1bb53756577
De vona C <devonacanada@gmail.com>
2024-11-13 16:39:43 -0500

    push prper folders

---
+++ 02-Challenge/React Portfolio/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+      <footer>
+        <a href="https://github.com/username" target="_blank">GitHub</a>
+        <a href="https://linkedin.com/in/username" target="_blank">LinkedIn</a>
+        <a href="https://stackoverflow.com/users/username" target="_blank">Stack Overflow</a>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
